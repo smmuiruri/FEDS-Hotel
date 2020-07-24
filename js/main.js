63d7c80ec9d339225c3d8849c7061877de166c1b
smuiruri <smmuiruri@gmail.com>
2020-07-24 04:32:41 -0700

    And date picker in javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-// booking section
+// booking section - increament & decreament on number of guests and rooms
 $(function ($) {
     $('#handleCounter1').handleCounter({
       minimum: 1,
       maximize: 100,
-  
-  
+    
     })
   
     $('#handleCounter2').handleCounter({
@@ -13,6 +12,28 @@ $(function ($) {
   
     })
   
+  });
+
+  // booking section - date
+
+  addEventListener('DOMContentLoaded', function () {
+
+
+
+
+    pickmeup('.range', {
+      mode: 'range',
+      separator: '    to   ',
+      position: 'bottom',
+      //        hide_on_select : true,
+      format: 'd b-y',
+  
+    });
+    //pickmeup('.range').show();
+  
   
   
   });
+
+  i
+  on
