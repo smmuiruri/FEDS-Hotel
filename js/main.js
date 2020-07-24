3b5b30ebc9c0e72aeb43c6afee31d3fb5468fb95
smuiruri <smmuiruri@gmail.com>
2020-07-24 03:26:15 -0700

    Add the counter section on the number of rooms and number of guests

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// booking section
+$(function ($) {
+    $('#handleCounter1').handleCounter({
+      minimum: 1,
+      maximize: 100,
+  
+  
+    })
+  
+    $('#handleCounter2').handleCounter({
+      minimum: 1,
+      maximize: 100,
+  
+    })
+  
+  
+  
+  });
